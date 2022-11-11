a4b31149e84e52879db4577bf0d59635a6137305
Egor Berdovskiy <dakebott@gmail.com>
2022-11-11 20:23:56 +0600

    Разбил бота по нескольким файлам

--- bot.py
+++ bot.py
@@ -1,37 +1,11 @@
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+from create_bot import dp
 
-import messages as msg
+from handlers import client, admin, other
 
-from config import TOKEN
+async def on_startup(_):
+    print('I am alive!')
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+client.register_handlers_client(dp)
 
-# Commands
-@dp.message_handler(commands=['start'])
-async def welcome(message: types.Message):
-    await bot.send_message(message.chat.id, f'Hello {message.from_user.full_name}! 🤖\nYou can ask me about the schedule or menu, a list of all the commands : /help')
-
-@dp.message_handler(commands=['help'])
-async def welcome(message: types.Message):
-    await bot.send_message(message.chat.id, '/schedule — Schedule of work\n/menu — Delicious burger menu\n/address — Addresses of our burger joints')
-
-@dp.message_handler(commands=['menu'])
-async def welcome(message: types.Message):
-    await bot.send_message(message.chat.id, msg.oops)
-
-@dp.message_handler(commands=['schedule'])
-async def welcome(message: types.Message):
-    await bot.send_message(message.chat.id, 'Schedule 🕓\nMo-Fr: 09:00-20:00am\nSa-Su: 10:00-19:00am')
-
-@dp.message_handler(commands=['address'])
-async def welcome(message: types.Message):
-    await bot.send_message(message.chat.id, msg.oops)
-
-# @dp.message_handler()
-# async def echo(message: types.Message):
-#    await bot.send_message(message.chat.id, message.text)
-
-executor.start_polling(dp, skip_updates=True)
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
---
+++ handlers/client.py
@@ -0,0 +1,26 @@
+from aiogram import types, Dispatcher
+from create_bot import bot, dp
+
+import messages as msg
+
+async def welcome(message: types.Message):
+    await bot.send_message(message.chat.id, f'Hello {message.from_user.full_name}! 🤖\nYou can ask me about the schedule or menu, a list of all the commands : /help')
+
+async def help(message: types.Message):
+    await bot.send_message(message.chat.id, '/schedule — Schedule of work\n/menu — Delicious burger menu\n/address — Addresses of our burger joints')
+
+async def menu(message: types.Message):
+    await bot.send_message(message.chat.id, msg.oops)
+
+async def schedule(message: types.Message):
+    await bot.send_message(message.chat.id, 'Schedule 🕓\nMo-Fr: 09:00-20:00am\nSa-Su: 10:00-19:00am')
+
+async def address(message: types.Message):
+    await bot.send_message(message.chat.id, msg.oops)
+
+def register_handlers_client(dp: Dispatcher):
+    dp.register_message_handler(welcome, commands=['start'])
+    dp.register_message_handler(help, commands=['help'])
+    dp.register_message_handler(menu, commands=['menu'])
+    dp.register_message_handler(schedule, commands=['schedule'])
+    dp.register_message_handler(address, commands=['address'])
